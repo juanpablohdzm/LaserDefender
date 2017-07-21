05175b9f0054d9f1cff0e91c4af337bfaa6f6f62
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-21 14:34:23 -0500

    PlayerController

---
+++ Assets/Scripts/PlayerController_CS.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController_CS : MonoBehaviour {
+
+    public float Speed=5f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            MoveToLeft();
+        if (Input.GetKey(KeyCode.RightArrow))
+            MoveToRight();
+        
+	}
+
+    void MoveToLeft()
+    {
+
+        transform.position += new Vector3(-Speed*Time.deltaTime, 0, 0);
+    }
+
+    void MoveToRight()
+    {
+        transform.position += new Vector3(Speed*Time.deltaTime, 0, 0);
+    }
+}
