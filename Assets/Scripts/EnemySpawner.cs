e75bd5e01924f1b76427afbf83cad03180e23406
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-21 18:24:21 -0500

    Moving Enemy formations

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -3,11 +3,18 @@
 public class EnemySpawner : MonoBehaviour {
 
     public GameObject EnemyPrefab;
+    public float Width = 10f;
+    public float Height = 10f;
+    public float Speed = 5;
+
+    private float MinX;
+    private float MaxX;
+    private bool HasHit = false;
 
 	// Use this for initialization
 	void Start () {
-       
 
+        GetScreenBounds();
         foreach(Transform child in transform)
         {
             GameObject EnemyClone = Instantiate(EnemyPrefab, child.position, Quaternion.identity);
@@ -15,9 +22,35 @@ public class EnemySpawner : MonoBehaviour {
         }
         
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height));
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (transform.position.x -.5f*Width < MinX || transform.position.x + .5f * Width > MaxX)
+            HasHit = !HasHit;
+        
+        Move();
+
 	}
+
+    void Move()
+    {
+        if (HasHit)
+        transform.position += Vector3.left*Speed * Time.deltaTime ;
+        else
+            transform.position +=Vector3.right*Speed * Time.deltaTime;
+    }
+   
+
+    void GetScreenBounds()
+    {
+        float Distance = transform.position.z - Camera.main.transform.position.z;
+        MinX= Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Distance)).x;
+        MaxX = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, Distance)).x;
+    }
 }
