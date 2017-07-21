693379530763064278734c272103cbafe45bc3ce
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-21 16:14:21 -0500

    Restricting the player´s position

--- Assets/Scripts/PlayerController_CS.cs
+++ Assets/Scripts/PlayerController_CS.cs
@@ -3,29 +3,41 @@
 public class PlayerController_CS : MonoBehaviour {
 
     public float Speed=5f;
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float ShipWidth = .5f;
+
+    float minX;
+    float maxX;
+
+    private void Start()
+    {
+        float Distance = transform.position.z - Camera.main.transform.position.z;
+        //Finds the vector from the relative origin of the camera to lower left corner
+        Vector3 WorldLeftPos=Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Distance));
+        Vector3 WorldRightPos = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, Distance));
+        minX = WorldLeftPos.x+ShipWidth;
+        maxX = WorldRightPos.x-ShipWidth;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKey(KeyCode.LeftArrow))
             MoveToLeft();
         if (Input.GetKey(KeyCode.RightArrow))
             MoveToRight();
+
+        float NewX = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector3(NewX, transform.position.y);
         
 	}
 
     void MoveToLeft()
     {
 
-        transform.position += new Vector3(-Speed*Time.deltaTime, 0, 0);
+        transform.position += Vector3.left * Speed * Time.deltaTime;
     }
 
     void MoveToRight()
     {
-        transform.position += new Vector3(Speed*Time.deltaTime, 0, 0);
+        transform.position += Vector3.right * Speed * Time.deltaTime;
     }
 }
