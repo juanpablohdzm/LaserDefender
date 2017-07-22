458c9afff84db597317bafe7d607e6e82592568d
DESKTOP-1MK8LI7\Juan Pablo <juanpablohdzm95@gmail.com>
2017-07-22 11:10:06 -0500

    Destroy Enemies

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float Health = 300;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile Laser = collision.gameObject.GetComponent<Projectile>();
+        if (Laser != null)
+        {
+            Health -= Laser.GetDamage();
+            Laser.Hit();
+        }
+        if (Health <= 0)
+            Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float Damage = 100;
+
+    public float GetDamage()
+    {
+        return Damage;
+    }
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
